6d6d2489d865d729279d1ff5f5f4b0975605901e
윤은식 <developer.lisiver@gmail.com>
2020-07-02 15:53:28 +0900

    feat(axios): added package 'axios' and try test in ssr-test.js

--- pages/ssr-test.js
+++ pages/ssr-test.js
@@ -1,16 +1,24 @@
 import Layout from '../components/Layout';
+import axios from 'axios';
 
 class SSRTest extends React.Component {
   static async getInitialProps ({req}) {
-    return req
-      ? { from: 'server' }
-      : { from: 'client' }
+    const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+    return {
+      users: response.data
+    }
   }
 
   render() {
+    const { users } = this.props;
+
+    const userList = users.map(
+      user => <li key={user.id}>{user.username}</li>
+    );
+
     return (
       <Layout>
-        {this.props.from}
+        {userList}
       </Layout>
     );
   };
